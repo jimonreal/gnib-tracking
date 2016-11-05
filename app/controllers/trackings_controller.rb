0b751cff57134b6f3473dbe64eacd04f9e0ce202
Bruno Bispo <me@brunobispo.com>
2016-11-05 20:58:42 +0000

    refact tracking controller

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,35 +1,46 @@
 class TrackingsController < ApplicationController
 
-  respond_to :html
+  before_action do
 
-  def new
     @tracking = Tracking.new
     @tracking.build_user
-  end
 
-  def avaibility_chart
-    availabilities = Availability.where created_at: (1.week.ago)..(1.seconds.ago)
+    @availabilities = Availability.where created_at: (1.week.ago)..(1.seconds.ago)
 
     if params[:tracking]
-      availabilities = availabilities.where(cat_id: params[:tracking][:cat_id]) if params[:tracking][:cat_id].present?
-      availabilities = availabilities.where(sbcat_id: params[:tracking][:sbcat_id]) if params[:tracking][:sbcat_id].present?
-      availabilities = availabilities.where(typ_id: params[:tracking][:typ_id]) if params[:tracking][:typ_id].present?
+      @availabilities = @availabilities.where(cat_id: params[:tracking][:cat_id]) if params[:tracking][:cat_id].present?
+      @availabilities = @availabilities.where(typ_id: params[:tracking][:typ_id]) if params[:tracking][:typ_id].present?
     end
 
-    render json: availabilities.group_by_hour(:created_at).count
+    @chart_data = @availabilities.group_by_day(:created_at).count
+    @current_availabilities = @availabilities.where.not(expired: true)
+
   end
 
-  def create
-    @tracking = Tracking.new tracking_params
-    success = @tracking.save
+  def new; end
 
-    respond_with @tracking
+  def preview; end
+
+  def create
+    @tracking = Tracking.create tracking_params
+
+    if @tracking.valid?
+      flash.now[:success] = I18n.t('flash.actions.create.success', resource_name: Tracking.model_name.human)
+      @tracking = Tracking.new
+      @tracking.build_user    
+    else
+      flash.now[:error] = I18n.t('flash.actions.create.error', resource_name: Tracking.model_name.human)
+    end
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   protected
 
     def tracking_params
-      params.require(:tracking).permit(:cat_id, :sbcat_id, :typ_id, user_attributes: [:email])
+      params.require(:tracking).permit(:cat_id, :sbcat_id, :typ_id, user_attributes: [:name, :email, :eula])
     end
 
 end

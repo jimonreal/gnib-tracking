4535b7a8a1051e30faecc8a317c629a082a651ea
Bruno Bispo <me@brunobispo.com>
2016-11-12 19:32:39 +0000

    send email on find availabilities

--- app/workers/seek_availabilities_worker.rb
+++ app/workers/seek_availabilities_worker.rb
@@ -2,9 +2,16 @@ class SeekAvailabilitiesWorker
   include Sidekiq::Worker
 
   def perform(data)
+  	cat = Cat.find(data['cat_id'])
+  	typ = Typ.find(data['typ_id'])
+    
     SeekAvailabilitiesService.new(
-      cat: Cat.find(data['cat_id']),
-    	typ: Typ.find(data['typ_id'])
+      cat: cat,
+    	typ: typ
     ).call
+
+    Tracking.where(cat: cat, typ: typ).where('created_at > ?', 1.month.ago).each do |tracking|
+    	AvailabilityMailer.alert(tracking).deliver_later unless tracking.new_availabilities.empty?
+    end
   end
 end
